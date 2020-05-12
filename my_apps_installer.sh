cbe06308eb44c1853e4540794ab01c4f161c4bbc
DevRenan <renan.gomes.fatecsp@gmail.com>
2020-05-11 23:23:08 -0300

    script de instalçao dos principais aplicativos no arch e no ubuntu

---
+++ my_apps_installer.sh
@@ -0,0 +1,50 @@
+#/bin/bash
+
+# Edite aqui os programas que deseja instalar com o gerenciador de pacotes universal SNAP
+PROGRAMAS_PARA_INSTALAR=(
+	rambox
+	firefox
+	chromium
+	dbeaver-ce
+	"code --classic"
+	postman
+	"teams-for-linux"
+	zoom-client
+  	"aws-cli --classic"
+)
+
+function install_snap_arch(){
+	sudo pacman -Syyu
+	sudo pacman -S git
+	#https://snapcraft.io/docs/installing-snapd
+	cd /tmp
+	git clone https://aur.archlinux.org/snapd.git
+	cd snapd
+	makepkg -si
+	sudo systemctl enable --now snapd.socket
+	sudo ln -s /var/lib/snapd/snap /snap
+}
+
+function install_snap_ubuntu(){
+	sudo apt update
+	sudo apt install snapd	
+}
+
+function test_snap(){
+	sudo snap install hello-world && hello-world
+}
+
+function install_snap(){
+	distro=$(cat /etc/*-release | grep ^ID= | cut -b4-)
+	install_snap_$distro
+	test_snap 	
+}
+
+function install_programs(){
+	for (( i = 0; i < ${#PROGRAMAS_PARA_INSTALAR[@]}; i++)) do
+		sudo snap install ${PROGRAMAS_PARA_INSTALAR[$i]}
+	done
+}
+
+install_snap
+install_programs
